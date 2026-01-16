4d221fbba8094ec906ae5756006529e52a535978
ihave5daysleft <ihave5daysleft@outlook.com>
2026-01-16 13:49:46 +0100

    Update MyFirstApp.py

--- MyFirstApp.py
+++ MyFirstApp.py
@@ -504,13 +504,15 @@ elif section == "🛠️ Tools":
             # Her er listen over alle mulige svar
                 possible_answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "HELL YEAH.", "Signs point to yes.", "⏃⌰⟟⟒⋏ ⌇⏃⊬⌇: ⊬⟒⌇.", "Try again.", "Ask again later.", "Better not tell you now.", "Network connection lost.", "Cannot predict now.", "Concentrate and ask again.", "⏃⌰⟟⟒⋏ ⌇⏃⊬⌇: ⟟ ⎅⍜⋏⏁ ☍⋏⍜⍙.", "How would i know?.", "Hmmm, I am not in the mood for this.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful.", "Hmmmm,no.", "Hell no.", "⏃⌰⟟⟒⋏ ⌇⏃⊬⌇: ⋏⍜."]
             answer = random.choice(possible_answers)
+            st.markdown("---")
             # Vis svaret flot
-                st.subheader("The Ball says:")
-                st.info(f"{answer} ⋆｡°✩")
+            st.subheader("The Ball says:")
+            st.info(f"{answer} ⋆｡°✩")
             
             # Ryd spørgsmålet hvis man vil (valgfrit)
             else:
                 st.error("You must ask a question first! I am not dumb.")
+
 
 
 
